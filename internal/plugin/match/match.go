419ed31effe8789cc499ef26ac3a0561f0b40cba
Aimer Neige <aimer.neige.soft@gmail.com>
2023-08-18 14:55:50 +0800

    about

--- internal/plugin/match/match.go
+++ internal/plugin/match/match.go
@@ -5,7 +5,11 @@ import (
 )
 
 func init() {
-	zero.OnFullMatch("老婆").Handle(func(ctx *zero.Ctx) {
+	engine := zero.New()
+	engine.OnFullMatch("老婆").Handle(func(ctx *zero.Ctx) {
 		ctx.Send("肥宅不要乱叫老婆啊！")
+	})
+	engine.OnFullMatchGroup([]string{"关于", "about"}).Handle(func(ctx *zero.Ctx) {
+		ctx.Send("新一代机器人\n🚧施工中🚧\n开源地址：https://github.com/aimerneige/yukichan-bot\n捐赠支持开发：https://aimer.aiursoft.cn/zh/donate/")
 	})
 }
