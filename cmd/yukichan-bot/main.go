1629796f34b87743390b7ae4c9ded0b67e8ec1b4
Aimer Neige <aimer.neige.soft@gmail.com>
2023-08-18 19:02:04 +0800

    music

--- cmd/yukichan-bot/main.go
+++ cmd/yukichan-bot/main.go
@@ -11,6 +11,7 @@ import (
 	_ "github.com/aimerneige/yukichan-bot/internal/plugin/fadian"
 	_ "github.com/aimerneige/yukichan-bot/internal/plugin/github"
 	_ "github.com/aimerneige/yukichan-bot/internal/plugin/match"
+	_ "github.com/aimerneige/yukichan-bot/internal/plugin/music"
 	_ "github.com/aimerneige/yukichan-bot/internal/plugin/read60s"
 	_ "github.com/aimerneige/yukichan-bot/internal/plugin/setu"
 	_ "github.com/aimerneige/yukichan-bot/internal/plugin/waifu"
