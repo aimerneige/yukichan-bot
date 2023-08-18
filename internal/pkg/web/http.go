5e9d4b51a7d68e7a7c2021c1c607765c9cfa6538
Aimer Neige <aimer.neige.soft@gmail.com>
2023-08-18 16:54:22 +0800

    util

---
+++ internal/pkg/web/http.go
@@ -0,0 +1,46 @@
+package web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func GetRequest(url string, queryList [][]string) ([]byte, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	q := req.URL.Query()
+	for _, queryItem := range queryList {
+		if len(queryItem) != 2 {
+			return nil, fmt.Errorf("%v is not a valid query", queryItem)
+		}
+		q.Add(queryItem[0], queryItem[1])
+	}
+	req.URL.RawQuery = q.Encode()
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
+func GetBytes(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
