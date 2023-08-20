bf9d3a4d4a6e87ec5764fa613ab485bf53ac7285
Aimer Neige <aimer.neige.soft@gmail.com>
2023-08-21 06:58:54 +0800

    fix

--- internal/plugin/alipay/alipay.go
+++ internal/plugin/alipay/alipay.go
@@ -1,9 +1,11 @@
 package alipay
 
 import (
+	b64 "encoding/base64"
 	"fmt"
 	"strings"
 
+	"github.com/FloatTech/floatbox/web"
 	zero "github.com/wdvxdr1123/ZeroBot"
 	"github.com/wdvxdr1123/ZeroBot/message"
 )
@@ -16,6 +18,11 @@ func init() {
 	zero.OnPrefixGroup([]string{"支付宝到账", "alipay"}).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
 			args := ctx.State["args"].(string)
-			ctx.SendChain(message.Record(fmt.Sprintf(api, strings.TrimSpace(args))))
+			mp3Url := fmt.Sprintf(api, strings.TrimSpace(args))
+			data, err := web.GetData(mp3Url)
+			if err != nil {
+				return
+			}
+			ctx.SendChain(message.Record("base64://" + b64.StdEncoding.EncodeToString(data)))
 		})
 }
