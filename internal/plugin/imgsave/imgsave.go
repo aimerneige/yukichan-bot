8658f50e6deb1ffee3335f090ac04ac2d683b569
Aimer Neige <aimer.neige.soft@gmail.com>
2024-09-05 23:36:07 +0800

    imgsave

---
+++ internal/plugin/imgsave/imgsave.go
@@ -0,0 +1,130 @@
+package imgsave
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+	"strings"
+	"time"
+
+	"github.com/aimerneige/yukichan-bot/internal/pkg/common"
+	log "github.com/sirupsen/logrus"
+	zero "github.com/wdvxdr1123/ZeroBot"
+)
+
+const imgRootDir = "./data/imgsave"
+
+func init() {
+	initTargetDir()
+	engine := zero.New()
+	common.DefaultSingle.Apply(engine)
+	engine.OnPrefix("加图").FirstPriority().SetBlock(true).Handle(handleImgSave)
+	engine.UseMidHandler(common.DefaultSpeedLimit)
+}
+
+func allowedImgList() [][]string {
+	return [][]string{
+		{"新田慧海", "新田恵海", "慧海", "恵海", "emi"},
+		{"高坂穗乃果", "高坂穂乃果 ", "穗乃果", "穂乃果", "ほのか", "honoka"},
+		{"表情包"},
+	}
+}
+
+func handleImgSave(ctx *zero.Ctx) {
+	msg := ctx.Event.Message
+	if len(msg) < 2 {
+		return
+	}
+	if msg[0].Type != "text" {
+		return
+	}
+	userSource := msg[0].Data["text"]
+	userTarget := strings.TrimSpace(userSource)
+	userTarget = strings.TrimPrefix(userTarget, "加图")
+	userTarget = strings.TrimSpace(userTarget)
+	userTarget = strings.ToLower(userTarget)
+	dirTarget := toTargetDirName(userTarget)
+	if dirTarget == "" {
+		return
+	}
+	imgList := []UserImage{}
+	for _, ele := range msg {
+		if ele.Type == "image" {
+			imgList = append(imgList, UserImage{
+				Name:   ele.Data["file_unique"],
+				Url:    ele.Data["url"],
+				Sender: ctx.Event.Sender.ID,
+			})
+		}
+	}
+	if len(imgList) < 1 {
+		return
+	}
+	successCount := 0
+	failCount := 0
+	targetDirPath := path.Join(imgRootDir, dirTarget)
+	for _, img := range imgList {
+		filePath := path.Join(targetDirPath, img.getRealFileName())
+		if downloadImage(img.Url, filePath) {
+			successCount++
+		} else {
+			failCount++
+		}
+	}
+	ctx.Send(fmt.Sprintf("任务完成，成功保存 %d 张图片，失败 %d 张", successCount, failCount))
+}
+
+type UserImage struct {
+	Name   string
+	Url    string
+	Sender int64
+}
+
+func (i UserImage) getRealFileName() string {
+	timeStr := time.Now().Format("2006-01-02-15-04-05")
+	return fmt.Sprintf("%s-%d-%s", timeStr, i.Sender, i.Name)
+}
+
+func downloadImage(imgUrl, filePath string) bool {
+	response, err := http.Get(imgUrl)
+	if err != nil {
+		log.Errorln("[imgsave] fail to download image", err)
+		return false
+	}
+	defer response.Body.Close()
+	file, err := os.Create(filePath)
+	if err != nil {
+		log.Errorln("[imgsave] fail to create image", err)
+		return false
+	}
+	defer file.Close()
+	_, err = io.Copy(file, response.Body)
+	if err != nil {
+		log.Errorln("[imgsave] fail to save image", err)
+		return false
+	}
+	return true
+}
+
+func toTargetDirName(s string) string {
+	for _, dir := range allowedImgList() {
+		for _, name := range dir {
+			if s == name {
+				return dir[0]
+			}
+		}
+	}
+	return ""
+}
+
+func initTargetDir() {
+	for _, dir := range allowedImgList() {
+		dirPath := path.Join(imgRootDir, dir[0])
+		_, err := os.Stat(dirPath)
+		if os.IsNotExist(err) {
+			os.Mkdir(dirPath, 0755)
+		}
+	}
+}
