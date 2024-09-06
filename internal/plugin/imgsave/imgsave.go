3eff7188784cbe1bc9e299d3fecb8b61884cb1b0
Aimer Neige <aimer.neige.soft@gmail.com>
2024-09-06 23:21:43 +0800

    updaet

--- internal/plugin/imgsave/imgsave.go
+++ internal/plugin/imgsave/imgsave.go
@@ -3,38 +3,50 @@ package imgsave
 import (
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
 	"os"
 	"path"
 	"strings"
 	"time"
 
+	b64 "encoding/base64"
+
 	"github.com/aimerneige/yukichan-bot/internal/pkg/common"
 	log "github.com/sirupsen/logrus"
 	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/message"
+	"gopkg.in/yaml.v3"
 )
 
-const imgRootDir = "./data/imgsave"
+const (
+	configFilePath = "./config/imgsave.yaml"
+)
+
+type ImgsaveConfig struct {
+	Rootdit   string     `yaml:"rootdir"`
+	Repos     [][]string `yaml:"repos"`
+	Blacklist []int64    `yaml:"blacklist"`
+}
+
+var config ImgsaveConfig
 
 func init() {
+	initConfig()
 	initTargetDir()
 	engine := zero.New()
 	common.DefaultSingle.Apply(engine)
 	engine.OnPrefix("加图").FirstPriority().SetBlock(true).Handle(handleImgSave)
-	engine.OnPrefix("创建图库", zero.OwnerPermission).SecondPriority().SetBlock(true).Handle(func(ctx *zero.Ctx) {})
-	engine.OnPrefix("来张").SecondPriority().SetBlock(true).Handle(func(ctx *zero.Ctx) {})
+	engine.OnPrefix("创建图库", zero.OwnerPermission).SecondPriority().SetBlock(true).Handle(handleRepoCreate)
+	engine.OnPrefix("来张").SecondPriority().SetBlock(true).Handle(handleImgGet)
 	engine.UseMidHandler(common.DefaultSpeedLimit)
 }
 
-func allowedImgList() [][]string {
-	return [][]string{
-		{"新田慧海", "新田恵海", "慧海", "恵海", "emi"},
-		{"高坂穗乃果", "高坂穂乃果 ", "穗乃果", "穂乃果", "ほのか", "honoka"},
-		{"表情包"},
-	}
-}
-
 func handleImgSave(ctx *zero.Ctx) {
+	if inBlacklist(ctx.Event.Sender.ID) {
+		ctx.Send("你已被拉黑！如有疑问请联系 bot 管理。")
+		return
+	}
 	msg := ctx.Event.Message
 	if len(msg) < 2 {
 		return
@@ -42,10 +54,10 @@ func handleImgSave(ctx *zero.Ctx) {
 	if msg[0].Type != "text" {
 		return
 	}
-	userTarget := parseUserTarget(msg[0].Data["text"])
+	userTarget := parseUserTarget(msg[0].Data["text"], "加图")
 	dirTarget := toTargetDirName(userTarget)
 	if dirTarget == "" {
-		ctx.Send(fmt.Sprintf("图片库「%s」不存在！可联系机器人管理员创建。", userTarget))
+		ctx.Send(fmt.Sprintf("图库「%s」不存在！可联系机器人管理员创建。", userTarget))
 		return
 	}
 	imgList := []UserImage{}
@@ -60,12 +72,12 @@ func handleImgSave(ctx *zero.Ctx) {
 	}
 	if len(imgList) < 1 {
 		ctx.Send("请发送图片！")
-		// TODO
+		// TODO 多轮对话
 		return
 	}
 	successCount := 0
 	failCount := 0
-	targetDirPath := path.Join(imgRootDir, dirTarget)
+	targetDirPath := path.Join(config.Rootdit, dirTarget)
 	for _, img := range imgList {
 		filePath := path.Join(targetDirPath, img.getRealFileName())
 		if downloadImage(img.Url, filePath) {
@@ -75,6 +87,29 @@ func handleImgSave(ctx *zero.Ctx) {
 		}
 	}
 	ctx.Send(fmt.Sprintf("任务完成，已存入「%s」。成功保存 %d 张图片，失败 %d 张", dirTarget, successCount, failCount))
+}
+
+func handleRepoCreate(ctx *zero.Ctx) {
+
+}
+
+func handleImgGet(ctx *zero.Ctx) {
+	userTarget := parseUserTarget(ctx.MessageString(), "来张")
+	dirTarget := toTargetDirName(userTarget)
+	if dirTarget == "" {
+		ctx.Send(fmt.Sprintf("图库「%s」不存在！可联系机器人管理员创建。", userTarget))
+		return
+	}
+	dirPath := path.Join(config.Rootdit, dirTarget)
+	imgB64, err := getLocalImage(dirPath)
+	if err != nil {
+		ctx.Send(fmt.Sprintf("读取本地图片失败了，错误信息：%v", err))
+		return
+	}
+	m := message.Image("base64://" + imgB64)
+	if id := ctx.Send(m).ID(); id == 0 {
+		ctx.Send("ERROR: 可能被风控或读取图片用时过长，请耐心等待")
+	}
 }
 
 type UserImage struct {
@@ -109,16 +144,16 @@ func downloadImage(imgUrl, filePath string) bool {
 	return true
 }
 
-func parseUserTarget(userSource string) string {
+func parseUserTarget(userSource, prefix string) string {
 	userTarget := strings.TrimSpace(userSource)
-	userTarget = strings.TrimPrefix(userTarget, "加图")
+	userTarget = strings.TrimPrefix(userTarget, prefix)
 	userTarget = strings.TrimSpace(userTarget)
 	userTarget = strings.ToLower(userTarget)
 	return userTarget
 }
 
 func toTargetDirName(s string) string {
-	for _, dir := range allowedImgList() {
+	for _, dir := range config.Repos {
 		for _, name := range dir {
 			if s == name {
 				return dir[0]
@@ -128,12 +163,59 @@ func toTargetDirName(s string) string {
 	return ""
 }
 
+func getLocalImage(dir string) (string, error) {
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		log.Errorln("[setu]", fmt.Sprintf("Fail to read dir %s", dir))
+		return "", err
+	}
+	if len(files) == 0 {
+		return "", fmt.Errorf("no image found in dir %s", dir)
+	}
+	imgFile := files[rand.Intn(len(files))]
+	// 检测是否读到文件夹，如果是则重试三次，否则报错
+	for i := 0; i < 3 && imgFile.IsDir(); i++ {
+		imgFile = files[rand.Intn(len(files))]
+	}
+	if imgFile.IsDir() {
+		return "", fmt.Errorf("fail to get a file in dir %s", dir)
+	}
+	imgBytes, err := os.ReadFile(path.Join(dir, imgFile.Name()))
+	if err != nil {
+		log.Errorln("[setu]", fmt.Sprintf("Fail to read img file %s", imgFile.Name()), err)
+		return "", err
+	}
+	return b64.StdEncoding.EncodeToString(imgBytes), nil
+}
+
+func inBlacklist(uid int64) bool {
+	for _, id := range config.Blacklist {
+		if uid == id {
+			return true
+		}
+	}
+	return false
+}
+
+func initConfig() {
+	confData, err := os.ReadFile(configFilePath)
+	if err != nil {
+		log.Panicln("[imgsace]", "fail to read config file!", err)
+		return
+	}
+	if err := yaml.Unmarshal(confData, &config); err != nil {
+		log.Panicln("[imgsave]", "Fail to unmarshal config data", err)
+		return
+	}
+	log.Infoln("[imgsave]", config)
+}
+
 func initTargetDir() {
-	for _, dir := range allowedImgList() {
-		dirPath := path.Join(imgRootDir, dir[0])
+	for _, dir := range config.Repos {
+		dirPath := path.Join(config.Rootdit, dir[0])
 		_, err := os.Stat(dirPath)
 		if os.IsNotExist(err) {
-			os.Mkdir(dirPath, 0755)
+			os.MkdirAll(dirPath, 0755)
 		}
 	}
 }
